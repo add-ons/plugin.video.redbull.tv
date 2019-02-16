e4e929fbfb76957719136075e3ff80b547017402
Calum Lind <calumlind@gmail.com>
2019-02-16 10:30:58 +0000

    Change addon content type to videos
    The 'movies' type was too specific and the Kodi docs recommend videos
    for this type of addon where there is various content.

--- plugin.video.redbulltv/default.py
+++ plugin.video.redbulltv/default.py
@@ -12,7 +12,7 @@ class RedbullTV2(object):
         self.base_url = sys.argv[0]
         self.addon_handle = int(sys.argv[1])
         self.args = urlparse.parse_qs(sys.argv[2][1:])
-        xbmcplugin.setContent(self.addon_handle, 'movies')
+        xbmcplugin.setContent(self.addon_handle, 'videos')
         self.redbulltv_client = redbulltv.RedbullTVClient(self.addon.getSetting('video.resolution'))
 
     @staticmethod
